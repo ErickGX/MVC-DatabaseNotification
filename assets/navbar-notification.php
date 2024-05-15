5d589f46ca79b71fc27cb33ae59b0e8fc853150e
ErickGX <ericksaopaulino1@hotmail.com>
2024-05-15 02:33:35 -0300

    adição logo senac navbar no centro de notificações

--- assets/navbar-notification.php
+++ assets/navbar-notification.php
@@ -1,11 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark bg-gradient">
     <div class="container-fluid">
         <a class="navbar-brand" href="/">Comunica Senac</a>
-        <a class="navbar-brand" href="/notification-center">Centro de notificação</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+          <a class="navbar-brand" href="#">
+                <img src="assets\images\senac-logo.png" alt="senac logo" width="100" height="50">
+             </a>
         <!-- Codigo do botao de sino com dropdown dinanimco aqui -->
         <div class="dropdown">
             <a href="#" class="dropdown-toggle" id="notificationDropdown" data-bs-toggle="dropdown">
